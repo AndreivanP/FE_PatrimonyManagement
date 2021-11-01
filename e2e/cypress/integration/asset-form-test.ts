688996237ea6e78a78da47c34750f549a79f1729
Andreivan <andreivan.santos@gmail.com>
2021-11-01 15:01:12 +0100

    Fix e2e tests

--- e2e/cypress/integration/asset-form-test.ts
+++ e2e/cypress/integration/asset-form-test.ts
@@ -1,6 +1,7 @@
 // / <reference types="cypress" />
 
 import faker from 'faker';
+const selectors = require('../support/selectors/asset-form');
 
 describe('Asset Form functionalities', () => {
 
@@ -36,8 +37,9 @@ describe('Asset Form functionalities', () => {
         let initialValue = `${faker.finance.amount()}`;
         let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
         let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
-        cy.createNewAsset(assetName, initialValue, companyName);
-        cy.get('.alert').contains('You have some incorrect data!');
+        cy.createNewAsset(assetName, initialValue, '', companyName);
+        cy.get(selectors.default.currentValue).then(($input) => {
+            expect($input[0].validationMessage).to.eq('Please fill out this field.')
+        })
     });
-
 });
