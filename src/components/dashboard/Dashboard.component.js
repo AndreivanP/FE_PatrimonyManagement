09dc69792769660ee1251bb2f6b4eedb038fc254
Andreivan <andreivan.santos@gmail.com>
2021-11-18 17:27:32 +0100

    Dynamic array dates

--- src/components/dashboard/Dashboard.component.js
+++ src/components/dashboard/Dashboard.component.js
@@ -6,6 +6,15 @@ import './Dashboard.css';
 import axios from "axios"
 import { API_URL } from "../../Properties"
 
+
+var getDaysArray = function(start, end) {
+    for(var arr=[],dt=new Date(start); dt<=end; dt.setDate(dt.getDate()+90)){
+        arr.push(new Date(dt).toLocaleDateString('en-ZA'));
+    }
+    arr.push(new Date().toLocaleDateString('en-ZA'));
+    return arr;
+};
+
 class DashboardComponent extends Component {
     constructor(props) {
         super(props)
@@ -14,16 +23,17 @@ class DashboardComponent extends Component {
             variableIncomeTotal: '',
             variableIncomePercentage: '',
             chartData: [],
-            datesInterval: ['2017/01/01', '2017/04/01', '2017/07/01', '2017/10/01',
-                            '2018/01/01', '2018/04/01', '2018/07/01', '2018/10/01',
-                            '2019/01/01', '2019/04/01', '2019/07/01', '2019/10/01',
-                            '2020/01/01', '2020/04/01', '2020/07/01', '2020/10/01',
-                            '2021/01/01', '2021/04/01', '2021/07/01', '2021/10/01',
-                            '2022/01/01'],
+                datesInterval: getDaysArray(new Date("2017-01-01"), new Date()),
+            // datesInterval: ['2017/01/01', '2017/04/01', '2017/07/01', '2017/10/01',
+            //     '2018/01/01', '2018/04/01', '2018/07/01', '2018/10/01',
+            //     '2019/01/01', '2019/04/01', '2019/07/01', '2019/10/01',
+            //     '2020/01/01', '2020/04/01', '2020/07/01', '2020/10/01',
+            //     '2021/01/01', '2021/04/01', '2021/07/01', '2021/10/01'],
             username: AuthenticationService.getLoggedInUserName(),
             token: AuthenticationService.getLoggedInToken()
         }
     }
+
 
     async componentDidMount() {
         AssetDataService.getCurrentTotal(this.state.username, this.state.token)
@@ -33,9 +43,26 @@ class DashboardComponent extends Component {
                 variableIncomePercentage: response.data.variable_income_percent
             }));
 
+            var daylist = getDaysArray(new Date("2017-01-01"), new Date());
+            console.log("xablau "+daylist)            
+        
+
         for (let i = 0; i < this.state.datesInterval.length; i++) {
             if (i !== this.state.datesInterval.length - 1) {
-                const response = await axios.get(`${API_URL}/users/${this.state.username}/assets-control?since=${this.state.datesInterval[i]}&till=${this.state.datesInterval[i + 1]}`,
+                const response = await axios.get(`${API_URL}/users/${this.state.username}/assets-control?since=${this.state.datesInterval[i]} 00:00:00&till=${this.state.datesInterval[i + 1]} 23:59:00`,
+                    { headers: { authorization: AuthenticationService.createJwtToken(this.state.token) } });
+                if (response.data != '') {
+                    this.setState(prevState => ({
+                        chartData: [...prevState.chartData, response.data[0].currentTotalValue]
+                    }))
+                } else {
+                    let lastValue = this.state.chartData.at(-1);
+                    this.setState(prevState => ({
+                        chartData: [...prevState.chartData, lastValue]
+                    }))
+                }
+            } else {
+                const response = await axios.get(`${API_URL}/users/${this.state.username}/assets-control?since=${this.state.datesInterval[i]} 00:00:00&till=${new Date().toLocaleDateString('en-ZA')} 23:59:00`,
                     { headers: { authorization: AuthenticationService.createJwtToken(this.state.token) } });
                 if (response.data != '') {
                     this.setState(prevState => ({
@@ -50,7 +77,6 @@ class DashboardComponent extends Component {
             }
         }
     }
-
 
     render() {
         const data = {
