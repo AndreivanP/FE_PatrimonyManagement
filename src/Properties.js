a93c1d129e4b85bdce36aa0525e29dac6993eefe
Andreivan <andreivan.santos@gmail.com>
2021-11-25 14:12:51 +0100

    Switch to a new be url

--- src/Properties.js
+++ src/Properties.js
@@ -1,3 +1,2 @@
-// export const API_URL = 'http://ec2-54-85-159-102.compute-1.amazonaws.com:8080'
 export const API_URL = 'http://asset-management:8080'
 export const BUILD_NUMBER = 'buildplaceholder'
