3e86a69ed552b29d04147f6185a26a59c83789aa
Andreivan <andreivan.santos@gmail.com>
2022-01-21 13:19:02 +0100

    Add parallel execution

--- e2e/cypress/integration/asset-form-test.ts
+++ e2e/cypress/integration/asset-form-test.ts
@@ -491,6 +491,260 @@ describe('Asset Form functionalities', () => {
  //       } 
     });
 
+
+    it('Create fixed income asset 41', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 42', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 43', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 44', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 45', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 46', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 47', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 48', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 49', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 50', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 51', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 52', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 53', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 54', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+
+    it('Create fixed income asset 55', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 56', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 57', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 58', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 58', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 59', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
+    it('Create fixed income asset 60', () => {
+        // for(let i=0; i <= 1; i++) {
+            let initialValue = `${faker.finance.amount()}`;
+            let currentValue = `${faker.finance.amount()}`;
+            let assetName = `Asset auto ${faker.lorem.word()} ${faker.datatype.number()}`;
+            let companyName = `Company ${faker.lorem.word()} ${faker.datatype.number()}`;
+            cy.createNewAsset(assetName, initialValue, currentValue, companyName);
+            cy.checkWhetherAssetIsCreated(assetName);
+            cy.visit('/users/Andreivan/assets/new');
+ //       } 
+    });
+
     it('Create variable income asset', () => {
         let initialValue = `${faker.finance.amount()}`;
         let currentValue = `${faker.finance.amount()}`;
