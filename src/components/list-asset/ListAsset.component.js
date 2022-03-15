0168d39a5b62d3e25da26fd40b6be952d72ccfd7
Andreivan <andreivan.santos@gmail.com>
2022-03-15 16:20:41 +0100

    Add remaning table config

--- src/components/list-asset/ListAsset.component.js
+++ src/components/list-asset/ListAsset.component.js
@@ -6,14 +6,18 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import {makeStyles} from '@material-ui/core/styles'
+import Avatar from '@mui/material/Avatar';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import TablePagination from '@mui/material/TablePagination';
+import TableFooter from '@mui/material/TableFooter';
+import { makeStyles } from '@material-ui/core/styles'
 import faker from '@faker-js/faker';
 
-
-let USERS =[]
+let USERS = []
 let STATUSES = ['Active', 'Peding', 'Blocked']
 
-for(let i=0; i<14; i++) {
+for (let i = 0; i < 14; i++) {
   USERS[i] = {
     name: faker.name.findName(),
     email: faker.internet.email(),
@@ -31,47 +35,114 @@ const useStyles = makeStyles((theme) => ({
   },
   tableContainer: {
     borderRadius: 15,
-    margin: '10px 10px',
+    margin: '10px 470px',
     maxWidth: 950
   },
   tableHeaderCell: {
     fontWeight: 'bold',
     backgroundColor: theme.palette.primary.dark,
     color: theme.palette.getContrastText(theme.palette.primary.dark)
+  },
+  avatar: {
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.getContrastText(theme.palette.primary.light)
+  },
+  name: {
+    fontWeight: 'bold',
+    color: theme.palette.secondary.dark
+  },
+  status: {
+    fontWeight: 'bold',
+    fontSize: '0.75rem',
+    color: 'white',
+    backgroundColor: 'grey',
+    borderRadius: 8,
+    padding: '3px 10px',
+    display: 'inline-block'
   }
 }));
 
-export default function ListAssetComponent() {
+function ListAssetComponent() {
   const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   return (
     <TableContainer component={Paper} className={classes.tableContainer}>
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
-          <TableRow>  
-            <TableCell className={classes.tableHeaderCell}>User Info Start form 7:22</TableCell>
+          <TableRow>
+            <TableCell style={{backgroundColor:'red', color: 'white', fontWeight: 'bold'}}>User Info</TableCell>
             <TableCell className={classes.tableHeaderCell}>Job Info</TableCell>
             <TableCell className={classes.tableHeaderCell}>Joining Date</TableCell>
             <TableCell className={classes.tableHeaderCell}>Status</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {USERS.map((row) => (
+          {USERS.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => (
             <TableRow key={row.name}>
               <TableCell>
-                {row.name}
-                {row.email}
-                {row.phone}
+                <Grid container>
+                  <Grid item lg={2}>
+                    <Avatar alt={row.name} src='.' className={classes.avatar} />
+                  </Grid>
+                  <Grid item lg={10}>
+                    <Typography className={classes.name}>{row.name}</Typography>
+                    <Typography color="textSecondary" variant="body2">{row.email}</Typography>
+                    <Typography color="textSecondary" variant="body2">{row.phone}</Typography>
+                  </Grid>
+                </Grid>
               </TableCell>
               <TableCell>
-                {row.jobTitle}
-                {row.company}
+                <Typography color="primary" variant="subtitle2">{row.jobTitle}</Typography>
+                <Typography color="textSecondary" variant="body2">{row.company}</Typography>
               </TableCell>
               <TableCell>{row.joinDate}</TableCell>
-              <TableCell>{row.status}</TableCell>
+              <TableCell>
+                <Typography
+                  className={classes.status}
+                  style={{
+                    backgroundColor:
+                      ((row.status === 'Active' && 'green') ||
+                        (row.status === 'Pending' && 'blue') ||
+                        (row.status === 'Blocked' && 'orange'))
+                  }}
+                >{row.status}</Typography>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={USERS.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'rows per page',
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+          </TableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
   );
 }
+
+export default ListAssetComponent;
